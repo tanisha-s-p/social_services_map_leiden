9f16e0c0050d688b4edcd266ec25239c6e209f3c
Tanisha Phadke <tanisha.phadke06@gmail.com>
2026-03-25 23:09:14 +0100

    fix: added files for hosting

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./IndexPage";
 import CustomerApp from "./CustomerApp";
 import DBManager from "./DBManager";
 
 export default function MainApp() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<IndexPage />} />
                 <Route path="/app" element={<CustomerApp />} />
                 <Route path="/admin" element={<DBManager />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
